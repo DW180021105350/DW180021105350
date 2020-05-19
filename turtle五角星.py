ac65daa00a514c4e12be40ebb44b97ca2d4a2029
DW180021105350 <1950668955@qq.com>
2020-05-19 15:25:08 +0800

    update hello.py. add turtle file.

---
+++ turtle五角星.py
@@ -0,0 +1,13 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from turtle import *
+>>> begin_fill()
+>>> fillcolor('red')
+>>> while True:
+	fd(200)
+	right(144)
+	if abs(pos())<1:
+		break
+
+	
+>>> end_fill()
